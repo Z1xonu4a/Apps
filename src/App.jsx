cda60a0abb4beb660116835a8308cfec297609c3
S1xonu4a <hedhet68@gmail.com>
2023-12-10 19:26:36 +0300

    upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+console.log("Hello Worold!")
+
+function App() {
+	return (
+		<div className='App'>
+			<div className={'Vlog'}>Hello Worold!</div>
+		</div>
+	)
+}
+
+export default App
